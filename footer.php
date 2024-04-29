94aacba2a15d871dddfc0ecd5efd162d9e33cfb7
Tariq-Mustafa <131915795+Tariq-Mustafa@users.noreply.github.com>
2024-04-29 19:08:41 +0300

    update footer.php

--- footer.php
+++ footer.php
@@ -2,18 +2,15 @@
 <html lang="en">
 	<head>
 		<style>
-			@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900");
 			footer
 			{
-				font-family: 'Poppins', sans-serif;
 				width: 100%;
-				margin-top: 5%;
+				margin-top: 10px;
 			}
 			footer p
 			{
 				color: black;
 				text-align: center;
-				padding: 20px 0px;
 				font-weight: 300;
 			}		
 		</style>
