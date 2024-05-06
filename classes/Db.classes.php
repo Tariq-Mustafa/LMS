bed2a2c2d30289ee4631160a507a20f39e8f1525
Anas Mohamed Alamir Osman Gad Al-Karim <125512181+AnasAlamir@users.noreply.github.com>
2024-05-07 02:10:13 +0300

    update Db.classes.php

--- classes/Db.classes.php
+++ classes/Db.classes.php
@@ -1,6 +1,12 @@
 <?php
 
 class Db {
+    private $dbHost="localhost";
+        private $dbUser="root";
+        private $dbPassword="";
+        private $dbName="se";
+        protected $connection;
+        
     protected function connect() {
         try {
             $username = "root";
@@ -13,4 +19,54 @@ class Db {
             die();
         }
     }
+    public function getConnection() { return $this->connection; }
+    public function openConnection()
+        {
+           $this->connection=new mysqli($this->dbHost,$this->dbUser,$this->dbPassword,$this->dbName);
+           if ($this->connection->connect_error) {
+            die("Connection failed: " . $this->connection->connect_error);
+            return false;
+          }
+          return true; 
+        }
+    public function closeConnection()
+    {
+        if($this->connection)
+        {
+            $this->connection->close();
+            return true;
+        }
+        return false;
+    }
+    public function select($qry)
+    {
+        $result=$this->connection->query($qry);
+        if(!$result)
+        {
+            echo "Error : ".mysqli_error($this->connection);
+            return false;
+        }
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
+    public function insert($qry)
+    {
+        $result=$this->connection->query($qry);
+        if(!$result)
+        {
+            echo "Error : ".mysqli_error($this->connection);
+            return false;
+        }
+        return $this->connection->insert_id;
+    }
+    public function delete($qry)
+    {
+        $result=$this->connection->query($qry);
+        if(!$result)
+        {
+            echo "Error : ".mysqli_error($this->connection);
+            return false;
+        }
+        return true;
+    }
+
 }
