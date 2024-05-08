b0a3a9ab14ee50bb5f0295bed1d2e294198fb094
Anas Mohamed Alamir Osman Gad Al-Karim <125512181+AnasAlamir@users.noreply.github.com>
2024-05-08 15:15:11 +0300

    still errMsg in manage not fixed

--- classes/Control/CourseController.php
+++ classes/Control/CourseController.php
@@ -1,86 +1,77 @@
 <?php
 require_once __DIR__ . '/../../classes/Db.classes.php';
 require_once __DIR__ . '/../Model/Course.php';
-class CourseController extends Db 
+class CourseController extends Db
 {
     public function getInstructors()
-        {
-            if($this->openConnection())
-            {
-                $query = "SELECT FacultyID, UserName FROM faculty_member JOIN user ON faculty_member.UserID = user.UserID";
-                return $this->select($query);
-            }
-            else
-            {
-                echo "Error in Database Connection";
-                return false;
-            }
+    {
+        if ($this->openConnection()) {
+            $query = "SELECT FacultyID, UserName FROM faculty_member JOIN user ON faculty_member.UserID = user.UserID";
+            return $this->select($query);
+        } else {
+            echo "Error in Database Connection";
+            return false;
         }
+    }
 
-        public function getCourses()
-        {
+    public function getCourses()
+    {
 
-            if($this->openConnection())
-            {
-                $query = "SELECT c.CourseName, u.UserName, c.Image,c.CourseID FROM course c JOIN faculty_member f ON c.InstructorID = f.FacultyID JOIN user u ON f.UserID = u.UserID";
-                return $this->select($query);
-            }
-            else
-            {
-                echo "Error in Database Connection";
-                return false;
-            }
+        if ($this->openConnection()) {
+            $query = "SELECT c.CourseName, u.UserName, c.Image,c.CourseID FROM course c JOIN faculty_member f ON c.InstructorID = f.FacultyID JOIN user u ON f.UserID = u.UserID";
+            return $this->select($query);
+        } else {
+            echo "Error in Database Connection";
+            return false;
         }
-        public function addCourse(Course $course)
-        {
+    }
+    public function addCourse(Course $course)
+    {
 
-            if ($this->openConnection()) {
-                $mysqli = $this->getConnection();
-                $description = $mysqli->real_escape_string($course->getDescription());
-                $query = "INSERT IGNORE INTO course 
+        if ($this->openConnection()) {
+            $mysqli = $this->getConnection();
+            $description = $mysqli->real_escape_string($course->getDescription());
+            $query = "INSERT IGNORE INTO course 
                           (CourseName, Description, InstructorID, Start_Date,End_Date, Image) 
                           VALUES 
                           ('{$course->getCourseName()}', '$description',
                            '{$course->getTeacherId()}', '{$course->getStartDate()}', '{$course->getEndDate()}',
                            '{$course->getImage()}')";
-                return $this->insert($query);
-            } else {
-                echo "Error in Database Connection";
-                return false;
-            }
+            return $this->insert($query);
+        } else {
+            echo "Error in Database Connection";
+            return false;
         }
-        public function deleteCourse(Course $course)
-        {
+    }
+    public function deleteCourse(Course $course)
+    {
+        if ($this->openConnection()) {
+            $query1 = "DELETE FROM student_course WHERE student_course.CourseID = '{$course->getCourseId()}'";
+            $result1 = $this->delete($query1);
+            
+            $query2 = "DELETE FROM course WHERE course.CourseName = '{$course->getCourseName()}'";
+            $result2 = $this->delete($query2);
+            return $result1 && $result2;
+        } else {
+            echo "Error in Database Connection";
+            return false;
+        }
+    }
+    
+    public function getNumberOfStudents(int $courseId)
+    {
+        if ($this->openConnection()) {
+            $query = "SELECT COUNT(*) as studentCount FROM student_course WHERE CourseID = '$courseId'";
+            $result = $this->select($query);
 
-            if($this->openConnection())
-            {
-                $query = "DELETE FROM course WHERE course.CourseName = '{$course->getCourseName()}'";
-                return $this->delete($query);
-            }
-            else
-            {
-                echo "Error in Database Connection";
-                return false;
-            }
-        }
-        public function getNumberOfStudents(int $courseId)
-        {
-            if ($this->openConnection()) {
-                $query = "SELECT COUNT(*) as studentCount FROM student_course WHERE CourseID = '$courseId'";
-                $result = $this->select($query);
-        
-                if (is_array($result) && isset($result[0]['studentCount'])) {
-                    return $result[0]['studentCount'];
-                } else {
-                    return 0;
-                }
+            if (is_array($result) && isset($result[0]['studentCount'])) {
+                return $result[0]['studentCount'];
             } else {
-                echo "Error in Database Connection";
-                return false;
+                return 0;
             }
+        } else {
+            echo "Error in Database Connection";
+            return false;
         }
-        
-        
+    }
 }
-
-?>
--- classes/Control/StudentController.php
+++ classes/Control/StudentController.php
@@ -16,7 +16,6 @@ class StudentController extends Db
     }
     public function addStudentToCourse(Student $student,Course $course){
         if ($this->openConnection()) {
-            $mysqli = $this->getConnection();
             $query = "INSERT IGNORE INTO student_course 
                       (StudentID, CourseID) 
                       VALUES 
