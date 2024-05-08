2d70abeb83f6a996a7b632f391f440f557ea257a
AkramGamal1 <133578647+AkramGamal1@users.noreply.github.com>
2024-05-09 01:17:01 +0300

    Add files via upload

---
+++ classes/Control/FeedbackController.php
@@ -0,0 +1,16 @@
+<?php
+require_once __DIR__ . '/../../classes/Db.classes.php';
+require_once __DIR__ . '/../Model/Feedback.php';
+
+class FeedbackController extends FeedbackModel {
+    private $model;
+
+    public function __construct(FeedbackModel $model) {
+        $this->model = $model;
+    }
+
+    public function submitFeedback($instructorID,$studentID, $content) {
+        return $this->model->submitFeedback($instructorID,$studentID, $content);
+    }
+}
+?>
