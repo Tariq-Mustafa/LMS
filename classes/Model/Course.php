f267e8f1c2548f90bd55298fda514950156799d7
Anas Mohamed Alamir Osman Gad Al-Karim <125512181+AnasAlamir@users.noreply.github.com>
2024-05-09 11:50:48 +0200

    Add files via upload

--- classes/Model/Course.php
+++ classes/Model/Course.php
@@ -73,25 +73,33 @@ class Course
         $phpCode = '<?php
 $courseName = "' . $course->getCourseName() . '";
 $description = "' . $course->getDescription() . '";
-$teacherId = ' . $course->getTeacherId() . ';
 $startDate = "' . $course->getStartDate() . '";
 $endDate = "' . $course->getEndDate() . '";
-$imagePath = "../../"."' . $course->getImage() . '";
+$imagePath = "' . $course->getImage() . '";
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/Dashboard.css">
     <title><?php echo $courseName; ?></title>
 </head>
 <body>
+<?php
+include_once "sidebar.php";
+?>
+</div>
+
+    <div class="details">
+        <div class="recentOrders">
     <h1><?php echo $courseName; ?></h1>
     <p><strong>Description:</strong> <?php echo $description; ?></p>
-    <p><strong>Teacher ID:</strong> <?php echo $teacherId; ?></p>
     <p><strong>Start Date:</strong> <?php echo $startDate; ?></p>
     <p><strong>End Date:</strong> <?php echo $endDate; ?></p>
     <img src="<?php echo $imagePath; ?>" alt="Course Image">
+    </div>
+    </div>
 </body>
 </html>
 ';
